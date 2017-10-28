643f64fae9ec240b752eb2622b383ecb90487d4e
quyen <chibedethuong91@gmail.com>
2017-10-28 23:50:15 +0700

    add comment

--- quyen.rb
+++ quyen.rb
@@ -1,9 +1,14 @@
+ I am quyen this is first commit
+# TOi sua o day
+Some one should edit this for me
+
+Code cua branch lam_ui
+
 # Team leader sua
 
- I am quyen this is first commit 
+ I am quyen this is first commit
 Some one should edit this for me
 
 Team leader them vai dong code cho master
 
 New member commit here
-
