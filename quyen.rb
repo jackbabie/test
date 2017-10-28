d8a90f3c82fbb60990fcdd82f5174f6a617e8625
jackbabie <doa9595@gmail.com>
2017-10-29 00:57:14 +0700

    add comment
    * add comment
    
    * fix review 1
    
    * fix review 2

--- quyen.rb
+++ quyen.rb
@@ -1,9 +1,20 @@
+ I am quyen this is first commit
+# TOi sua o day
+Some one should edit this for me
+
+# Toi sua lan 2
+Code cua branch lam_ui
+
 # Team leader sua
 
- I am quyen this is first commit 
+ I am quyen this is first commit
 Some one should edit this for me
 
 Team leader them vai dong code cho master
 
 New member commit here
 
+Sau khi co nguoi khac commit tren master
+
+# Them dong nay
+Add sau khi co thanh vien moi
