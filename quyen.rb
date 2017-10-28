7fd474d03a0ca9c1e4ac0e17f575d19dc0b6669a
quyen <chibedethuong91@gmail.com>
2017-10-28 23:54:13 +0700

    fix review 1

--- quyen.rb
+++ quyen.rb
@@ -2,6 +2,7 @@
 # TOi sua o day
 Some one should edit this for me
 
+# Toi sua lan 2
 Code cua branch lam_ui
 
 # Team leader sua
@@ -12,3 +13,7 @@ Some one should edit this for me
 Team leader them vai dong code cho master
 
 New member commit here
+
+Sau khi co nguoi khac commit tren master
+
+# Them dong nay
